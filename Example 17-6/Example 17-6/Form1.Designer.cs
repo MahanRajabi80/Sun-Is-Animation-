a26e3a5cc91cb68b59ca05f668ca4dfda6b493e8
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 19:38:36 +0330

    Add files via upload

---
+++ Example 17-6/Example 17-6/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Example_17_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(449, 335);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Example 17-6 (Sun is Animation)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Example 17-6/Example 17-6/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_17_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //--------------------------------------------------------------------------
+        const int RADIUS = 50;
+        int X = 180, Y = 100;
+        //--------------------------------------------------------------------------
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics gr = this.CreateGraphics();
+            gr.Clear(Color.Black);
+            gr.FillEllipse(Brushes.Yellow, X, Y, RADIUS * 2, RADIUS*2);
+            float x1, y1, x2, y2;
+            float r1 = RADIUS, r2 = r1;
+            float teta;
+            Random r = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i <= 360; i += r.Next(20) + 1)
+            {
+                teta = (float)(Math.PI * i / 180);
+                x1 = (float)(r1 * Math.Cos(teta));
+                y1 = (float)(r1 * Math.Sin(teta));
+                int m = r.Next(50);
+                r2 = r1 + m;
+                x2 = (float)(r2 * Math.Cos(teta));
+                y2 = (float)(r2 * Math.Sin(teta));
+                gr.DrawLine(new Pen(Color.White), X + RADIUS + x1, Y + RADIUS + y1, X + RADIUS + x2, Y + RADIUS + y2);
+            }
+        }
+    }
+}
